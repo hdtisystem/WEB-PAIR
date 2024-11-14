23df8a1723fbb26caedac85b542a0b088b32e746
hdtisystem <123359175+hdtisystem@users.noreply.github.com>
2024-11-14 08:05:52 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,6 +1,6 @@
 const mega = require("megajs");
 const auth = {
-    email: 'tharinduindrajith230@gmail.com',
+    email: 'hdtimusic@gmail.com',
     password: 'Thari230230230',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
